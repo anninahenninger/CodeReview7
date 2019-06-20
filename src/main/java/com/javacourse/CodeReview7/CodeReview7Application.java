daea28f606197381d7a6ce644b753b76cc7f294c
Annina Henninger <annina.henninger@aon.at>
2019-06-20 10:49:55 +0200

    initial commit with content

---
+++ src/main/java/com/javacourse/CodeReview7/CodeReview7Application.java
@@ -0,0 +1,13 @@
+package com.javacourse.CodeReview7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeReview7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeReview7Application.class, args);
+	}
+
+}
