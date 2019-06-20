be7581eb8d050ad6e70d316e54c949269b1219fd
Annina Henninger <annina.henninger@aon.at>
2019-06-20 11:31:39 +0200

    adds WebMvcConfig class

--- src/main/java/com/javacourse/CodeReview7/configuration/WebMvcConfig.java
+++ src/main/java/com/javacourse/CodeReview7/configuration/WebMvcConfig.java
@@ -1,4 +1,14 @@
 package com.javacourse.CodeReview7.configuration;
 
-public class WebMvcConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
 }
